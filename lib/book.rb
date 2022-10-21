9b0c922cb64a92faa44cb2869cce0df41bf0b449
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-10-21 06:24:58 +0300

    Complete assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
 
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    # attr_accessor :author
+    # attr_accessor :page_count
 
+    def initialize(title="And Then There Were None", author)
+        @title = title        
+    end
+
+    # def author
+    #     @author = author
+    # end
+    def turn_page
+        # @turn_page = turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+end
+
+setbook = Book.new( "no")
+# setbook.title
+# setbook.author = ('yes')
